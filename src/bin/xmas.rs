9924e443e65baf2753d13b1d2e5ac1f8eaf4b77d
altunenes <enesaltun2@gmail.com>
2024-12-27 20:59:13 +0300

    single pass shader: xmas tree

---
+++ src/bin/xmas.rs
@@ -0,0 +1,338 @@
+use cuneus::{Core,ShaderApp, ShaderManager, UniformProvider, UniformBinding, BaseShader,ExportSettings, ExportError, ExportManager};
+use winit::event::*;
+use image::ImageError;
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct ShaderParams {
+    lambda: f32,    // top light
+    theta: f32,     // dec_bleed
+    alpha: f32,     // ao
+    sigma: f32,     // star
+    gamma: f32,     // light_r
+    blue: f32,      // light_b
+    a: f32,         // light_g
+    b: f32,         // bgr
+    c: f32,         // bgb
+    d: f32,         // bgg
+    e: f32,         // light2
+    f: f32,         // light3
+    g: f32,         // light1
+    aa: f32,        // gamma
+    iter: f32,      // ao_dist
+    bound: f32,     // vig
+    tt: f32,        // starglow
+}
+
+
+impl UniformProvider for ShaderParams {
+    fn as_bytes(&self) -> &[u8] {
+        bytemuck::bytes_of(self)
+    }
+}
+
+struct ChristmasShader {
+    base: BaseShader,
+    params_uniform: UniformBinding<ShaderParams>,
+}
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+    let (app, event_loop) = ShaderApp::new("Christmas Tree Shader", 800, 600);
+    let shader = ChristmasShader::init(app.core());
+    app.run(event_loop, shader)
+}
+impl ChristmasShader {
+    fn capture_frame(&mut self, core: &Core, time: f32) -> Result<Vec<u8>, wgpu::SurfaceError> {
+        let settings = self.base.export_manager.settings();
+        let (capture_texture, output_buffer) = self.base.create_capture_texture(
+            &core.device,
+            settings.width,
+            settings.height
+        );
+        let align = 256;
+        let unpadded_bytes_per_row = settings.width * 4;
+        let padding = (align - unpadded_bytes_per_row % align) % align;
+        let padded_bytes_per_row = unpadded_bytes_per_row + padding;
+        let capture_view = capture_texture.create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = core.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+            label: Some("Capture Encoder"),
+        });
+        self.base.time_uniform.data.time = time;
+        self.base.time_uniform.update(&core.queue);
+        {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Capture Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &capture_view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color::BLACK),
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                depth_stencil_attachment: None,
+                timestamp_writes: None,
+                occlusion_query_set: None,
+            });
+            render_pass.set_pipeline(&self.base.renderer.render_pipeline);
+            render_pass.set_vertex_buffer(0, self.base.renderer.vertex_buffer.slice(..));
+            if let Some(texture_manager) = &self.base.texture_manager {
+                render_pass.set_bind_group(0, &texture_manager.bind_group, &[]);
+            }
+            render_pass.set_bind_group(1, &self.base.time_uniform.bind_group, &[]);
+            render_pass.set_bind_group(2, &self.params_uniform.bind_group, &[]);
+            render_pass.draw(0..4, 0..1);
+        }
+        encoder.copy_texture_to_buffer(
+            wgpu::ImageCopyTexture {
+                texture: &capture_texture,
+                mip_level: 0,
+                origin: wgpu::Origin3d::ZERO,
+                aspect: wgpu::TextureAspect::All,
+            },
+            wgpu::ImageCopyBuffer {
+                buffer: &output_buffer,
+                layout: wgpu::ImageDataLayout {
+                    offset: 0,
+                    bytes_per_row: Some(padded_bytes_per_row),
+                    rows_per_image: Some(settings.height),
+                },
+            },
+            wgpu::Extent3d {
+                width: settings.width,
+                height: settings.height,
+                depth_or_array_layers: 1,
+            },
+        );
+        core.queue.submit(Some(encoder.finish()));
+        let buffer_slice = output_buffer.slice(..);
+        let (tx, rx) = std::sync::mpsc::channel();
+        buffer_slice.map_async(wgpu::MapMode::Read, move |result| {
+            tx.send(result).unwrap();
+        });
+        core.device.poll(wgpu::Maintain::Wait);
+        rx.recv().unwrap().unwrap();
+        let padded_data = buffer_slice.get_mapped_range().to_vec();
+        let mut unpadded_data = Vec::with_capacity((settings.width * settings.height * 4) as usize);
+        for chunk in padded_data.chunks(padded_bytes_per_row as usize) {
+            unpadded_data.extend_from_slice(&chunk[..unpadded_bytes_per_row as usize]);
+        }
+        Ok(unpadded_data)
+    }
+
+    fn save_frame(&self, mut data: Vec<u8>, frame: u32, settings: &ExportSettings) -> Result<(), ExportError> {
+        let frame_path = settings.export_path
+            .join(format!("frame_{:05}.png", frame));
+        
+        if let Some(parent) = frame_path.parent() {
+            std::fs::create_dir_all(parent)?;
+        }
+
+        // Convert BGRA to RGBA
+        for chunk in data.chunks_mut(4) {
+            chunk.swap(0, 2);
+        }
+
+        let image = image::ImageBuffer::<image::Rgba<u8>, Vec<u8>>::from_raw(
+            settings.width,
+            settings.height,
+            data
+        ).ok_or_else(|| ImageError::Parameter(
+            image::error::ParameterError::from_kind(
+                image::error::ParameterErrorKind::Generic(
+                    "Failed to create image buffer".to_string()
+                )
+            )
+        ))?;
+        
+        image.save(&frame_path)?;
+        Ok(())
+    }
+
+    fn handle_export(&mut self, core: &Core) {
+        if let Some((frame, time)) = self.base.export_manager.try_get_next_frame() {
+            if let Ok(data) = self.capture_frame(core, time) {
+                let settings = self.base.export_manager.settings();
+                if let Err(e) = self.save_frame(data, frame, settings) {
+                    eprintln!("Error saving frame: {:?}", e);
+                }
+            }
+        } else {
+            self.base.export_manager.complete_export();
+        }
+    }
+}
+impl ShaderManager for ChristmasShader {
+    fn init(core: &Core) -> Self {
+        let time_bind_group_layout = core.device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            entries: &[wgpu::BindGroupLayoutEntry {
+                binding: 0,
+                visibility: wgpu::ShaderStages::FRAGMENT,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Uniform,
+                    has_dynamic_offset: false,
+                    min_binding_size: None,
+                },
+                count: None,
+            }],
+            label: Some("time_bind_group_layout"),
+        });
+
+        let params_bind_group_layout = core.device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            entries: &[wgpu::BindGroupLayoutEntry {
+                binding: 0,
+                visibility: wgpu::ShaderStages::FRAGMENT,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Uniform,
+                    has_dynamic_offset: false,
+                    min_binding_size: None,
+                },
+                count: None,
+            }],
+            label: Some("params_bind_group_layout"),
+        });
+
+        let params_uniform = UniformBinding::new(
+            &core.device,
+            "Params Uniform",
+            ShaderParams {
+                sigma: 2.0,
+                gamma: 1.05,
+                blue: 0.4,
+                a: 0.45,
+                b: 0.02,
+                c: 0.01,
+                d: 0.03,
+                e: 0.0,
+                f: 0.0,
+                g: 0.0,
+                lambda: 1.77,
+                theta: 0.002,
+                alpha: 0.1,
+                aa: 0.1,
+                iter: 0.1,
+                bound: 0.1,
+                tt: 0.4,
+            },
+            &params_bind_group_layout,
+            0,
+        );
+
+        let bind_group_layouts = vec![
+            &time_bind_group_layout,
+            &params_bind_group_layout,
+        ];
+
+        let base = BaseShader::new(
+            core,
+            include_str!("../../shaders/vertex.wgsl"),
+            include_str!("../../shaders/xmas.wgsl"),
+            &bind_group_layouts,
+            None,
+        );
+
+        Self {
+            base,
+            params_uniform,
+        }
+    }
+
+    fn update(&mut self, core: &Core) {
+        self.base.update_time(&core.queue);
+        if self.base.export_manager.is_exporting() {
+            self.handle_export(core);
+        }
+    }
+
+    fn render(&mut self, core: &Core) -> Result<(), wgpu::SurfaceError> {
+        let output = core.surface.get_current_texture()?;
+        let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
+        
+        let mut params = self.params_uniform.data;
+        let mut changed = false;
+        let mut should_start_export = false;
+        let mut export_request = self.base.export_manager.get_ui_request();
+
+        let full_output = if self.base.key_handler.show_ui {
+            self.base.render_ui(core, |ctx| {
+                egui::Window::new("Christmas Tree Settings").show(ctx, |ui| {
+                    changed |= ui.add(egui::Slider::new(&mut params.lambda, 0.0..=5.0).text("top light")).changed();
+                    changed |= ui.add(egui::Slider::new(&mut params.theta, -0.01..=0.01).text("dec_bleed")).changed();
+                    changed |= ui.add(egui::Slider::new(&mut params.alpha, -0.4..=0.4).text("ao")).changed();
+                    changed |= ui.add(egui::Slider::new(&mut params.sigma, 0.0..=12.0).text("star")).changed();
+                    changed |= ui.add(egui::Slider::new(&mut params.gamma, 0.0..=3.0).text("light_r")).changed();
+                    changed |= ui.add(egui::Slider::new(&mut params.blue, 0.0..=3.0).text("light_b")).changed();
+                    changed |= ui.add(egui::Slider::new(&mut params.a, 0.0..=3.0).text("light_g")).changed();
+                    changed |= ui.add(egui::Slider::new(&mut params.b, 0.0..=0.2).text("bgr")).changed();
+                    changed |= ui.add(egui::Slider::new(&mut params.c, 0.0..=0.2).text("bgb")).changed();
+                    changed |= ui.add(egui::Slider::new(&mut params.d, 0.0..=0.2).text("bgg")).changed();
+                    changed |= ui.add(egui::Slider::new(&mut params.g, 0.0..=20.0).text("light1")).changed();
+                    changed |= ui.add(egui::Slider::new(&mut params.e, 0.002..=20.0).text("light2")).changed();
+                    changed |= ui.add(egui::Slider::new(&mut params.f, 0.0..=20.0).text("light3")).changed();
+                    changed |= ui.add(egui::Slider::new(&mut params.iter, -24.0..=24.0).text("ao_dist")).changed();
+                    changed |= ui.add(egui::Slider::new(&mut params.bound, 0.00001..=0.5).text("vig")).changed();
+                    changed |= ui.add(egui::Slider::new(&mut params.aa, 0.0..=1.0).text("gamma")).changed();
+                    changed |= ui.add(egui::Slider::new(&mut params.tt, -0.5..=0.5).text("starglow")).changed();
+                    
+                    should_start_export = ExportManager::render_export_ui_widget(ui, &mut export_request);
+                });
+            })
+        } else {
+            self.base.render_ui(core, |_ctx| {})
+        };
+
+        self.base.export_manager.apply_ui_request(export_request);
+
+        if changed {
+            self.params_uniform.data = params;
+            self.params_uniform.update(&core.queue);
+        }
+
+        if should_start_export {
+            self.base.export_manager.start_export();
+        }
+
+        let mut encoder = core.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+            label: Some("Render Encoder"),
+        });
+
+        {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Main Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color::BLACK),
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                depth_stencil_attachment: None,
+                timestamp_writes: None,
+                occlusion_query_set: None,
+            });
+
+            render_pass.set_pipeline(&self.base.renderer.render_pipeline);
+            render_pass.set_vertex_buffer(0, self.base.renderer.vertex_buffer.slice(..));
+            render_pass.set_bind_group(0, &self.base.time_uniform.bind_group, &[]);
+            render_pass.set_bind_group(1, &self.params_uniform.bind_group, &[]);
+            render_pass.draw(0..4, 0..1);
+        }
+
+        self.base.handle_render_output(core, &view, full_output, &mut encoder);
+        core.queue.submit(Some(encoder.finish()));
+        output.present();
+
+        Ok(())
+    }
+
+    fn handle_input(&mut self, core: &Core, event: &WindowEvent) -> bool {
+        if self.base.egui_state.on_window_event(core.window(), event).consumed {
+            return true;
+        }
+        if let WindowEvent::KeyboardInput { event, .. } = event {
+            return self.base.key_handler.handle_keyboard_input(core.window(), event);
+        }
+        false
+    }
+}
+
