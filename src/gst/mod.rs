02a64dbf5777919cec23d928f9e9bb615118d925
altunenes <enesaltun2@gmail.com>
2025-03-09 22:15:29 +0300

    more deeper gstreamer debugs for my sanity checks

--- src/gst/mod.rs
+++ src/gst/mod.rs
@@ -3,7 +3,10 @@ pub mod video;
 use log::info;
 
 pub fn init() -> anyhow::Result<()> {
-    info!("Initializing GStreamer subsystem");
+    // Set very verbose logging for spectrum and element messages
+    std::env::set_var("RUST_LOG", "info,warn,debug,gstreamer=debug,cuneus=debug,spectrum=trace");
+    std::env::set_var("GST_DEBUG", "spectrum:5,bus:5,message:5");
+    info!("Setting up GStreamer with enhanced logging");
     gstreamer::init()?;
     info!("GStreamer initialized successfully");
     Ok(())
