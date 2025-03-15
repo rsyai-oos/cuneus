b068041c7902df29d33c3100ea4b74a1a38164ff
altunenes <enesaltun2@gmail.com>
2025-03-15 12:56:33 +0300

    better egui on droste with transperant window

--- src/bin/droste.rs
+++ src/bin/droste.rs
@@ -347,6 +347,10 @@ impl ShaderManager for SpiralShader {
         let video_info = self.base.get_video_info();
         let full_output = if self.base.key_handler.show_ui {
             self.base.render_ui(core, |ctx| {
+                // transparent
+                ctx.style_mut(|style| {
+                    style.visuals.window_fill = egui::Color32::from_rgba_premultiplied(0, 0, 0, 180);
+                });
                 egui::Window::new("Shader Settings").show(ctx, |ui| {
                     egui::CollapsingHeader::new("Media").default_open(true).show(ui, |ui| {
                         ShaderControls::render_media_panel(
